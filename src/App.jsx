d9670364cb08679b59b20bbd94db2094961f5fc5
ahmed mohamed <ahmed.muhamed.tech@gmail.com>
2026-04-05 13:18:29 +0200

    initial setup

---
+++ src/App.jsx
@@ -0,0 +1,8 @@
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+const queryClient = new QueryClient();
+
+function App() {
+  return <QueryClientProvider client={queryClient}></QueryClientProvider>;
+}
+
+export default App;
